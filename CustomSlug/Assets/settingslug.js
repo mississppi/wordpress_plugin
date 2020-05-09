fb19e22c9c7cc9a878513db1a38a1430d321ac98
mississppi <saku@mbp.local>
2020-05-09 17:52:08 +0900

    delete plugin

--- CustomSlug/Assets/settingslug.js
+++
@@ -1,7 +0,0 @@
-jQuery(document).ready(function($){
-    var date = new Date();
-
-    var formatted = `${date.getFullYear()}${("0"+(date.getMonth()+1)).slice(-2)}${("0"+(date.getDate())).slice(-2)}${("0"+(date.getHours())).slice(-2)}${("0"+(date.getMinutes())).slice(-2)}${("0"+(date.getSeconds())).slice(-2)}`.replace(/\n|\r/g, '');
-
-    $('#post_name').val(formatted);
-});
